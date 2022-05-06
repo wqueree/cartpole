f169ac6fa071125b3a3768cbafcddfa9f0b66250
Will Queree <wqueree@gmail.com>
2022-05-06 15:38:18 +0100

    Added naïve policy function

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import gym
 
 
+def naive_action(observation):
+    return 0 if observation[2] < 0 else 1
+
+
 def main():
+    policy = naive_action
     env = gym.make("CartPole-v0")
-    env.reset()
-    for _ in range(1000):
-        env.render()
-        env.step(env.action_space.sample())
+    for i_episode in range(20):
+        observation = env.reset()
+        for t in range(100):
+            env.render()
+            action = policy(observation)
+            observation, reward, done, info = env.step(action)
+            if done:
+                print(f"Episode finished after {t + 1} timesteps")
+                break
     env.close()
 
 
